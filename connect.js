72a99172794d8be3cd3babaa8a9d3b1c12cde1b7
Aliosseily <ali.ossaily@outlook.com>
2018-05-10 15:57:52 +0300

    dealing with database using node.js

---
+++ connect.js
@@ -0,0 +1,54 @@
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('moviesdatabase.db' , sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the mydatabase SQlite database.');
+  });
+
+
+  db.all('SELECT * FROM movies' ,[], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      console.log(row.id +"\t"+ row.name +"\t"+ row.genre);
+    });
+  });
+
+
+  db.run(`INSERT INTO movies(id,name,genre) VALUES(9,'movie','action')`, function(err) {
+    if (err) {
+      return console.log(err.message);
+    }
+    // get the last insert id
+    console.log(`A row has been inserted with id ${this.lastID}`);
+  });
+
+
+  db.run('UPDATE movies set name="kingsman",genre="animation" where id = 8', function(err) {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log(`Row(s) updated: ${this.changes}`);
+   
+  });
+
+
+  db.run(`DELETE FROM movies WHERE id=?`, 9, function(err) {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log(`Row(s) deleted ${this.changes}`);
+  });
+
+ 
+
+  	
+  db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Close the mydatabase connection.');
+  });
